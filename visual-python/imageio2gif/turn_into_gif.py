23a93332e72c8b30d44c66ba924292144be14270
LHJ Piper <2940596912@qq.com>
2020-03-03 20:59:28 +0800

    fix turn-gif for more arguments

--- visual-python/imageio2gif/turn_into_gif.py
+++ visual-python/imageio2gif/turn_into_gif.py
@@ -7,11 +7,14 @@
 
 import imageio
 import os
+import sys
 
 # DURATION time each png owns
 DURATION = 0.5
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        DURATION = sys.argv[1]
     # path init
     path = os.path.join(__file__, "..")
     # read pngs
@@ -20,7 +23,7 @@ if __name__ == "__main__":
     for file in file_list:
         if file[-4:]!=".png":
             png_list.remove(file)
-    png_list.sort(key=lambda x: int(x[:-4]))
+    png_list.sort(key=lambda x: float(x[:-4]))
     # now all the pngs are in the png_list
     # and sorted well from min to max
     # turn pngs into a gif file
@@ -34,5 +37,5 @@ if __name__ == "__main__":
     # production path
     gif_path = os.path.join(path, "my_gif.gif")
     imageio.mimsave(gif_path, frames, 'GIF', duration=DURATION)
-    print("done")
+    print("done, duration of each pic is " + str(DURATION))
     pass
